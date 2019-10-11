732d2fe5aa994da9d7aec994e439e2a2845815fb
Joe Warren <joe.warren7@protonmail.com>
2019-10-11 06:24:13 -0500

    Updates

---
+++ precache-manifest.25320718cfb4d8d543c76265f6b2947f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e74b03b47e34cf58a1a3145d089a49c0",
+    "url": "/reactjs-feed/index.html"
+  },
+  {
+    "revision": "cb88149dbba8264c051c",
+    "url": "/reactjs-feed/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "05aea4940141d2af82da",
+    "url": "/reactjs-feed/static/js/2.81a3d13d.chunk.js"
+  },
+  {
+    "revision": "cb88149dbba8264c051c",
+    "url": "/reactjs-feed/static/js/main.42758d76.chunk.js"
+  },
+  {
+    "revision": "fdee532f0672295a080f",
+    "url": "/reactjs-feed/static/js/runtime-main.930c9974.js"
+  }
+]);
