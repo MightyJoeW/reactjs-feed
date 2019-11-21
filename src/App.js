a960535050ad7260c35e939cfb6b60fbd0465868
Joe Warren <joe.warren7@protonmail.com>
2019-11-21 05:54:39 -0600

    fix: add test props for presentation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import React, { lazy, Suspense } from 'react';
+// EXTERNAL DEPENDENCIES
+import React, { lazy, Suspense, useState, useEffect } from 'react';
+import axios from 'axios';
 
-const ErrorBoundary = lazy(() => import('./components/error-boundary/error-boundary'));
+// INTERNAL DEPENDENCIES
 const ReactThread = lazy(() => import('./components/react-thread/react-thread'));
 
+// LOCAL VARIABLES
+const subReddit = 'reactjs';
+
+// COMPONENT DEFINITION
 const App = () => {
+  const [feed, setFeed] = useState([]);
+  const [baseUrl] = useState('https://www.reddit.com');
+
+  // const [userName] = useState('Joe');
+  // const [isUserLoggedIn] = useState(false);
+
+  useEffect(() => {
+    axios.get(`https://www.reddit.com/r/${subReddit}.json`).then(res => {
+      setFeed(res.data.data.children)
+    })
+  }, []);
+
   return (
     <>
       <h1> /r/reactjs </h1>
       <Suspense fallback={<div>Loading...</div>}>
-        <ErrorBoundary>
-          <ReactThread />
-        </ErrorBoundary>
+        <ReactThread
+          baseUrl={baseUrl}
+          feed={feed}
+        // isUserLoggedIn={isUserLoggedIn}
+        // userName={userName}
+        />
       </Suspense>
     </>
   );
--- src/components/react-thread/react-thread.jsx
+++ src/components/react-thread/react-thread.jsx
@@ -1,30 +1,26 @@
-import React, { Fragment, useState, useEffect } from 'react';
-import axios from 'axios';
+// EXTERNAL DEPENDENCIES
+import React, { Fragment } from 'react';
 
-const subreddit = 'reactjs';
-
-const ReactThread = () => {
-  const [feed, setFeed] = useState([]);
-  const [baseUrl] = useState('https://www.reddit.com');
-
-  useEffect(() => {
-    axios.get(`https://www.reddit.com/r/${subreddit}.json`).then(res => {
-      setFeed(res.data.data.children)
-    })
-  }, []);
-
+// COMPONENT DEFINITION
+const ReactThread = props => {
   return (
-    <ul>
-      {feed.map(post => {
-        return (
-          <Fragment key={post.data.id}>
-            <li>
-              <p><a href={`${baseUrl}${post.data.permalink}`}>{post.data.title}</a></p>
-            </li>
-          </Fragment>
-        )
-      })}
-    </ul>
+    <>
+      // {props.isUserLoggedIn && <p>Hello {props.userName}</p>}
+      <ul>
+        {props.feed.length > 0
+          ? props.feed.map(post => {
+            return (
+              <Fragment key={post.data.id}>
+                <li>
+                  <p><a href={`${props.baseUrl}${post.data.permalink}`}>{post.data.title}</a></p>
+                </li>
+              </Fragment>
+            )
+          })
+          : 'Feed unavailable. Try again later.'
+        }
+      </ul>
+    </>
   )
 }
 
